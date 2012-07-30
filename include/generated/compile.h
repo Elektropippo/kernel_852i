9af7e2141d018d558bbaca42eb594c98b1ddc299
Elektropippo <mms@my-laptop.(none)>
2012-07-30 22:42:42 +0200

    First Version. display,touch and wifi work camera, gsensor ko.

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 14 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#14 PREEMPT Sun Jul 29 18:27:29 CEST 2012"
+#define LINUX_COMPILE_BY "mms"
+#define LINUX_COMPILE_HOST "my-laptop"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
